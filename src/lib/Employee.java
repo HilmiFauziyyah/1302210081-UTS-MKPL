8c107eab63732759baa5d018be8055f6b283c64a
hilmifauziyyah <hilmifs2113@gmail.com>
2024-04-21 15:13:41 +0700

    Menambahkan variabel dan Menghapus Method yang tidak berguna
    Menambahkan Variabel isMarried dan menghapus method yang tidak digunakan

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -1,7 +1,7 @@
 package lib;
 
 import java.time.LocalDate;
-import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,35 +12,33 @@ public class Employee {
 	private String idNumber;
 	private String address;
 
-	private int yearJoined;
-	private int monthJoined;
-	private int dayJoined;
+	private LocalDate DateJoined;
 	private int monthWorkingInYear;
 
-	private boolean isForeigner;
+	private boolean Foreigner;
 	private boolean gender; // true = Laki-laki, false = Perempuan
+	private boolean isMarried;
 
 	private int monthlySalary;
 	private int otherMonthlyIncome;
 	private int annualDeductible;
 
-	private String spouseName;
-	private String spouseIdNumber;
-
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 
 	public Employee(String employeeId, String EmployeeName, String idNumber, String address,
-			int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+			String DateJoined, boolean Foreigner, boolean isMarried, boolean gender) {
 		this.employeeId = employeeId;
 		this.EmployeeName = EmployeeName;
 		this.idNumber = idNumber;
 		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
-		this.isForeigner = isForeigner;
+		this.Foreigner = Foreigner;
+		this.isMarried = isMarried;
 		this.gender = gender;
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		this.DateJoined = LocalDate.parse(DateJoined, formatter);
+		;
 
 		childNames = new LinkedList<String>();
 		childIdNumbers = new LinkedList<String>();
@@ -57,17 +55,17 @@ public class Employee {
 	public void setMonthlySalary(int grade) {
 		if (grade == 1) {
 			monthlySalary = 3000000;
-			if (isForeigner) {
+			if (Foreigner) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
 		} else if (grade == 2) {
 			monthlySalary = 5000000;
-			if (isForeigner) {
+			if (Foreigner) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
 		} else if (grade == 3) {
 			monthlySalary = 7000000;
-			if (isForeigner) {
+			if (Foreigner) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
 		}
@@ -79,11 +77,6 @@ public class Employee {
 
 	public void setAdditionalIncome(int income) {
 		this.otherMonthlyIncome = income;
-	}
-
-	public void setSpouse(String spouseName, String spouseIdNumber) {
-		this.spouseName = spouseName;
-		this.spouseIdNumber = idNumber;
 	}
 
 	public void addChild(String childName, String childIdNumber) {
@@ -101,13 +94,13 @@ public class Employee {
 
 		LocalDate date = LocalDate.now();
 
-		if (date.getYear() == yearJoined) {
-			monthWorkingInYear = date.getMonthValue() - monthJoined;
+		if (date.getYear() == DateJoined.getYear()) {
+			monthWorkingInYear = date.getMonthValue() - DateJoined.getMonthValue();
 		} else {
 			monthWorkingInYear = 12;
 		}
 
 		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible,
-				spouseIdNumber.equals(""), childIdNumbers.size());
+				isMarried, childIdNumbers.size());
 	}
 }
