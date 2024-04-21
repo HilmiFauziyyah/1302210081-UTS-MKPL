9ed80be1d1490b6193ecfc86b7a5587c381f5142
hilmifauziyyah <hilmifs2113@gmail.com>
2024-04-21 14:26:26 +0700

    Mengubah Varia yang tidak bermanfaat / berlebihan
    Mengubah penggunaan Variabel dengan objek atau penamaan yang lebih deksriptif agar lebih mudah pada saat penggunaannya

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -8,8 +8,7 @@ import java.util.List;
 public class Employee {
 
 	private String employeeId;
-	private String firstName;
-	private String lastName;
+	private String EmployeeName;
 	private String idNumber;
 	private String address;
 
@@ -31,11 +30,10 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address,
+	public Employee(String employeeId, String EmployeeName, String idNumber, String address,
 			int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
 		this.employeeId = employeeId;
-		this.firstName = firstName;
-		this.lastName = lastName;
+		this.EmployeeName = EmployeeName;
 		this.idNumber = idNumber;
 		this.address = address;
 		this.yearJoined = yearJoined;
